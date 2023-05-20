7d441f0a0f4cb9fa55439a2f1fbcd20c3e9481f0
indaleen <inquinsayas@gmail.com>
2023-05-20 16:49:44 +0800

    array example

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,17 +1,71 @@
-﻿namespace HelloWorld
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //using implicit data type var
-            var booktitles = new string[4];
+            //data type
+            //strongly-type
 
-            //populating the elements of the array
-            booktitles[0] = "Brain Rules";
-            booktitles[1] = "Pragmatic Programmer";
-            booktitles[2] = "Harry Potter and the Half-Blood Prince";
-            booktitles[3] = "The Alchemist";
+            string[] booktitles; //declaration
+
+            Console.WriteLine("how many book do you want to save?");
+            int numberOfBooks = Convert.ToInt16(Console.ReadLine());
+
+            booktitles = new string[numberOfBooks]; //initialization
+
+            //booktitles[0] = "Deep Work";
+            //booktitles[3] = "Brain Rules";
+            //booktitles[2] = "Harry Potter";
+
+            for (int i = 0; i < numberOfBooks; i++)
+            {
+                Console.Write($"Input the title of Book {i + 1} :");
+                var title = Console.ReadLine();
+
+                booktitles[i] = title;
+            }
+
+            //Console.WriteLine(booktitles[3]);
+            //Console.WriteLine(booktitles[4]);
+
+            foreach(var book in booktitles)
+            {
+                Console.WriteLine(book) ;
+            }
+
+            for (int i = 3; i < booktitles.Length - 1 ; i++)
+            {
+                booktitles[i] = booktitles[i] + "!!!";
+                Console.WriteLine(booktitles[i]);
+            }
+
+
+            Console.WriteLine("Enter the title of the book that you want to search");
+            Console.Write("Book Title: ");
+            var searchTitle = Console.ReadLine();
+
+            var result = Array.IndexOf(booktitles, searchTitle);
+            //returns -1 if the item being searched is not existing in the array,
+            //else it will return the position index of the item
+
+            if (result != -1)
+            {
+                Console.WriteLine($"The index of the {searchTitle} is {result}.");
+                booktitles.SetValue(searchTitle.ToUpper(), result);
+            }
+            else
+            {
+                Console.WriteLine("Item not found!");
+            }
+
+            foreach (var book in booktitles)
+            {
+                Console.WriteLine(book);
+            }
         }
     }
 }
