453e3238ddec55c62b293c1fb389819ce8a475c0
indaleen <inquinsayas@gmail.com>
2023-05-20 18:12:09 +0800

    implemented List

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,63 +5,81 @@ namespace HelloWorld
 {
     internal class Program
     {
+        static List<string> booktitles = new List<string>();
         static void Main(string[] args)
         {
-            //data type
-            //strongly-type
+            //List<string> booktitles = new List<string>();
 
-            string[] booktitles; //declaration
+            booktitles.Add("Brain Rules");
+            booktitles.Add("Deep Work");
+            booktitles.Add("The Spy");
+            booktitles.Add("The Code");
+            booktitles.Insert(4, "Start with Why");
 
-            Console.WriteLine("how many book do you want to save?");
-            int numberOfBooks = Convert.ToInt16(Console.ReadLine());
+            booktitles.Remove("Deep");
 
-            booktitles = new string[numberOfBooks]; //initialization
+            Console.WriteLine("Before AddRange: ");
+            DisplayListContents();
 
-            //booktitles[0] = "Deep Work";
-            //booktitles[3] = "Brain Rules";
-            //booktitles[2] = "Harry Potter";
-
-            for (int i = 0; i < numberOfBooks; i++)
-            {
-                Console.Write($"Input the title of Book {i + 1} :");
-                var title = Console.ReadLine();
-
-                booktitles[i] = title;
-            }
-
-            //Console.WriteLine(booktitles[3]);
-            //Console.WriteLine(booktitles[4]);
-
-            foreach(var book in booktitles)
-            {
-                Console.WriteLine(book) ;
-            }
-
-            for (int i = 3; i < booktitles.Length - 1 ; i++)
-            {
-                booktitles[i] = booktitles[i] + "!!!";
-                Console.WriteLine(booktitles[i]);
-            }
+            //for (int i = 0; i < booktitles.Count; i++)
+            //{
+            //    booktitles[i] = booktitles[i].ToUpper();
+            //    Console.WriteLine(booktitles[i]);
+            //}
 
 
-            Console.WriteLine("Enter the title of the book that you want to search");
-            Console.Write("Book Title: ");
+            var books = new List<string>() { "Code 1", "Code 2", "Code 3" };
+            books.Add("Book 4");
+
+            booktitles.AddRange(books);
+
+            Console.WriteLine("\nAfter AddRange(): ");
+            DisplayListContents();
+
+            Console.WriteLine($"The list contains {booktitles.Count} books");
+
+            //Console.Write("\nEnter the book title that you want to search: ");
+            //var searchTitle = Console.ReadLine();
+
+            //if (booktitles.Contains(searchTitle))
+            //{
+            //    Console.WriteLine("Item found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Item not found");
+            //}
+
+            Console.Write("\nEnter the book title that you want to search: ");
             var searchTitle = Console.ReadLine();
+            var result = booktitles.Find(x => x == searchTitle);
 
-            var result = Array.IndexOf(booktitles, searchTitle);
-            //returns -1 if the item being searched is not existing in the array,
-            //else it will return the position index of the item
-
-            if (result != -1)
+            if (result != null)
             {
-                Console.WriteLine($"The index of the {searchTitle} is {result}.");
-                booktitles.SetValue(searchTitle.ToUpper(), result);
+                Console.WriteLine("Item foundfound");
             }
             else
             {
-                Console.WriteLine("Item not found!");
+                Console.WriteLine("Item not found");
             }
 
+            booktitles.Sort();
+            Console.WriteLine("\nAfter Sort()");
+            DisplayListContents();
+
+            booktitles.Reverse();
+            Console.WriteLine("\nAfter Reverse()");
+            DisplayListContents();
+
+            booktitles.RemoveRange(2, 3);
+            Console.WriteLine("\nAfter RemoveRange()");
+            DisplayListContents();
+
+            Console.WriteLine($"The list contains {booktitles.Count} books");
+        }
+
+        static void DisplayListContents()
+        {
             foreach (var book in booktitles)
             {
                 Console.WriteLine(book);
